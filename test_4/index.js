6b7ccfcda6dc15a9b8da0a2d84d03af9f48dc39e
Ali Hamza <hamzamashooq492@gmail.com>
2024-01-13 22:50:58 +0500

    If-Else condition

---
+++ test_4/index.js
@@ -0,0 +1,12 @@
+// 4. Write a ts program to check whether a number is
+//  divisible by 5 and 11 or not.
+var num=45
+if (num %5 ==0) {
+    console.log("it is divisible by 5");
+}
+else if (num %11 ==0) {
+    console.log("it is divisible by 11");
+} 
+else {
+    console.log("not divisible by both 5 and 11");
+}
