fc1dd7360be39363031519e0bb22f1c86162adc5
Ali Hamza <hamzamashooq492@gmail.com>
2024-01-13 22:49:28 +0500

    If-Else condition

---
+++ test_3/index.js
@@ -0,0 +1,8 @@
+// 3. Write a ts program to check whether a number is negative, positive or zero.
+var num=-10
+if (num > 0) {
+    console.log("positive integer");
+} 
+else {
+    console.log("negative integer");
+}
